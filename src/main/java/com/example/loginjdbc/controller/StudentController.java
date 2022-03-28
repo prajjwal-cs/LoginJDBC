12fb83a0ce2f799ce26f969ce00ebd9398978103
Prajjwal <prajjwal199275@gmail.com>
2022-03-28 23:24:01 +0530

    Create a StudentController.java class in controller package

---
+++ src/main/java/com/example/loginjdbc/controller/StudentController.java
@@ -0,0 +1,13 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 28-03-2022
+Time: 23:12
+File: StudentController.java 
+
+*/
+
+package com.example.loginjdbc.controller;
+
+public class StudentController {
+}
